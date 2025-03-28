d5d9bb05f97e8ab21bc3f49095637e6efe5f9bad
robcarmof <robcarmof@gmail.com>
2025-03-28 15:33:37 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -55,7 +55,7 @@ data "google_project" "current" {}
 resource "google_project_iam_member" "gke_nodes_artifact_access" {
   project = data.google_project.current.project_id
   role    = "roles/artifactregistry.reader"
-  member  = "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com"
+  member  = "serviceAccount:${google_service_account.gke_nodes.email}"
 }
 
 resource "google_service_account" "gke_nodes" {
