d46ee8750489444baf62f4013b912a2a73fc7cd7
robcarmof <robcarmof@gmail.com>
2025-04-10 10:03:12 -0300

    fixing variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,6 @@
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
-  default     = "gen-lang-client-0683956833"  # Default value, can be overridden
 }
 
 variable "region" {
@@ -19,11 +18,16 @@ variable "zone" {
 variable "cluster_name" {
   description = "Name of the GKE cluster"
   type        = string
-  default     = "basic-gke-cluster"
+  default     = "gke-cluster"
 }
 
 variable "node_count" {
   description = "Number of nodes in the GKE cluster"
   type        = number
   default     = 1
+}
+
+variable "tfstate_bucket_name" {
+  type        = string
+  description = "Name of the GCS bucket for Terraform state"
 }
